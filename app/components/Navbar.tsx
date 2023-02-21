72ed643d64bd8e688b05c96ec7cce08887a4ecc0
dehwyy <91pony@mail.ru>
2023-02-21 16:56:04 +0300

    navbar remake

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,33 @@
 'use client'
-import Image from "next/image";
-import cloud from "../../public/cloud.png"
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import DehazeIcon from '@mui/icons-material/Dehaze';
 import Link from "next/link";
 import Home from '@mui/icons-material/HomeOutlined';
-import {useState} from "react";
+import {useState, useRef} from "react";
+import {CSSTransition} from "react-transition-group";
 const Navbar = () => {
+    const [isExpanded, setExpanded] = useState(false)
+    const nodeRef = useRef(null)
     return (
-        <div className="fixed right-0 top-0 select-none h-[100px] ">
-            <Image src={cloud} alt={"Navbar"} width={200} height={200}/>
-            <div className="absolute top-6 right-6 flex justify-center">
-                    <div className="flex gap-x-3">
+        <div className="fixed right-0 top-0 select-none h-[100px]">
+            <div className="absolute right-0 flex justify-center z-50">
+                    <div className="flex gap-x-3 bg-custom-blue p-[32.5px]">
                         <Link href="/">
                             <Home sx={{cursor: "pointer"}} fontSize="large" />
                         </Link>
                         <Link href="/user/1">
                             <PermIdentityIcon sx={{cursor: "pointer"}} fontSize="large"/>
                         </Link>
-                        <DehazeIcon sx={{cursor: "pointer"}} fontSize="large" />
+                        <DehazeIcon sx={{cursor: "pointer"}} fontSize="large" onClick={() => setExpanded(p => !p)}/>
                     </div>
             </div>
+            <CSSTransition
+                in={isExpanded}
+                nodeRef={nodeRef}
+                timeout={1000}
+                classNames="navbar">
+                <div ref={nodeRef} className={(isExpanded? "w-screen visible" : "w-0") + " h-[100px] bg-custom-blue"}>123</div>
+            </CSSTransition>
         </div>
     );
 };
