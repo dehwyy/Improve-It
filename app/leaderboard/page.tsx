9b5e8a8542dbdcc15e9e7015986a57a3c614431d
dehwyy <91pony@mail.ru>
2023-03-15 03:55:39 +0300

    new leaderboard page

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -3,29 +3,27 @@ import LeaderboardUser from '@/app/leaderboard/components/LeaderboardUser'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import Heading from '@/app/leaderboard/components/Heading'
+import AlphaPageWrapper from '@/app/components/UI/Wrappers/_AlphaPageWrapper'
 import DefaultPageWrapper from '@/app/components/UI/Wrappers/DefaultPageWrapper'
-
 const Page = async () => {
   const users = await selectByCorrectAnswered()
   const currentUser = await getServerSession(authOptions)
   return (
     <DefaultPageWrapper>
-      <>
-        <Heading />
-        <div className="flex-auto flex flex-col gap-y-5 mt-5">
-          {users &&
-            users.map(user => (
-              <LeaderboardUser
-                id={user.id}
-                name={user.name as string}
-                image={user.image as string}
-                correctAnswered={user.correctAnswered}
-                answered={user.answered}
-                currentUserImage={currentUser?.user?.image}
-              />
-            ))}
-        </div>
-      </>
+      <Heading />
+      <div className="flex flex-col gap-y-5 my-5 w-[90%] mx-auto">
+        {users &&
+          users.map(user => (
+            <LeaderboardUser
+              id={user.id}
+              name={user.name as string}
+              image={user.image as string}
+              correctAnswered={user.correctAnswered}
+              answered={user.answered}
+              currentUserImage={currentUser?.user?.image}
+            />
+          ))}
+      </div>
     </DefaultPageWrapper>
   )
 }
