b7d42d53ffcfb45e69d5e220f13b7365468ee2b1
dehwyy <91pony@mail.ru>
2023-03-17 01:05:22 +0300

    fixed shadows on styled buttons

--- app/components/UI/Wrappers/StyleWrapper.tsx
+++ app/components/UI/Wrappers/StyleWrapper.tsx
@@ -20,7 +20,6 @@ const StyledWrapper = styled.div`
   transition-duration: 200ms;
   background-color: rgb(51, 51, 51);
   border-width: 2px;
-  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 `
 
 export default StyledWrapper
