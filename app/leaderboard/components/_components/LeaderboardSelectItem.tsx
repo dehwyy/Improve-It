8e7dccb7183daccd1bb23d966598b5ad985f5456
dehwyy <91pony@mail.ru>
2023-03-29 01:41:46 +0300

    remove user-select on sortTypes

--- app/leaderboard/components/_components/LeaderboardSelectItem.tsx
+++ app/leaderboard/components/_components/LeaderboardSelectItem.tsx
@@ -10,7 +10,7 @@ const LeaderboardSelectItem = ({ text, onClick }: IProps) => {
   const isHover = useHover(hoverRef)
   return (
     <>
-      <div ref={hoverRef} onClick={onClick} className="cursor-pointer font-bold text-xl flex items-center justify-center text-center">
+      <div ref={hoverRef} onClick={onClick} className="select-none cursor-pointer font-bold text-xl flex items-center justify-center text-center">
         <span className={`${isHover ? 'text-[1.3rem] text-red-400' : 'text-blue-400'} duration-500 transition-all`}>{text}</span>
       </div>
     </>
