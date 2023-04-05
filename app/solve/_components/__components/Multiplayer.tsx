6989b05a0a5d456bbf1cfd8276a4009cc0c73add
dehwyy <91pony@mail.ru>
2023-04-06 00:51:49 +0300

    test commit

--- app/solve/_components/__components/Multiplayer.tsx
+++ app/solve/_components/__components/Multiplayer.tsx
@@ -15,7 +15,7 @@ const Multiplayer = ({ gameType, userId }: IProps) => {
   return (
     <ModeTypeWrapper mode={gameType}>
       <ImagesWrapper>
-        <Image src="/images/chad.png" alt="blueface" width="200" height="200" className="rounded" />
+        <Image src="/images/11.png" alt="blueface" width="200" height="200" className="rounded" />
       </ImagesWrapper>
       <TextWrapper>
         <p className="text-3xl text-center">Multiplayer for 2+ player. The fastest player wins.</p>
