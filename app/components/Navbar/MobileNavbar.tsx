2b443aaf85a6fcf136f4206c1df5d9e489ba5230
dehwyy <91pony@mail.ru>
2023-04-24 19:31:24 +0300

    fixed mobileNavbar r-padding:

--- app/components/Navbar/MobileNavbar.tsx
+++ app/components/Navbar/MobileNavbar.tsx
@@ -21,7 +21,7 @@ const MobileNavbar = ({ userId }: IProps) => {
       <TopPageLoader />
       <ClickAwayListener onClickAway={() => setExpanded(false)}>
         <nav className={`text-blue-500 transition-all text-[3rem] flex flex-col items-center z-50 fixed right-0 top-0`}>
-          <div className="p-[1rem] pb-0 z-50 ">
+          <div className="p-[1rem] pb-0 z-50 !pr-7">
             <MenuIcon
               onClick={() => setExpanded(p => !p)}
               fontSize="large"
@@ -29,7 +29,7 @@ const MobileNavbar = ({ userId }: IProps) => {
             />
           </div>
           <Slide in={isExpanded} direction="left" unmountOnExit mountOnEnter>
-            <div className="bg-[#333333] border-l-2 border-gray-300 fixed bottom-0 top-0 pt-20 z-40 pr-4 pl-2">
+            <div className="bg-[#333333] border-l-2 border-gray-300 fixed bottom-0 top-0 pt-20 z-40 pr-6 pl-2">
               <Icons userId={userId} isGrowable={isExpanded} />
             </div>
           </Slide>
